f8313869e1407cb27744d8868c34934c09817562
Vladislav Famtsov <gggwp4@gmail.com~>
2023-07-06 17:11:16 +0300

    +photo background at mainActivity

--- JavaApp/app/src/main/java/com/example/javaapp/MainActivity.java
+++ JavaApp/app/src/main/java/com/example/javaapp/MainActivity.java
@@ -4,21 +4,44 @@ package com.example.javaapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
 
 
 public class MainActivity extends QuitDialog {
 
     float x1, x2, y1, y2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final View activityView = findViewById(R.id.activity_view);
+        activityView.setOnTouchListener(new View.OnTouchListener() {
+            float dY = 0;
 
-        }
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        dY = view.getY() - event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        // Перемещаем активити вниз за жестом свайпа
+                        if (event.getRawY() + dY > 0) {
+                            activityView.setY(event.getRawY() + dY);
+                        }
+                        break;
+                    default:
+                        return false;
+                }
+                return true;
+            }
+        });
+    }
 
 
-    public boolean onTouchEvent(MotionEvent touchevent){
-        switch (touchevent.getAction()){
+    public boolean onTouchEvent(MotionEvent touchevent) {
+        switch (touchevent.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 x1 = touchevent.getX();
                 y1 = touchevent.getY();
@@ -26,7 +49,7 @@ public class MainActivity extends QuitDialog {
             case MotionEvent.ACTION_UP:
                 x2 = touchevent.getX();
                 y2 = touchevent.getY();
-                if (y1 + 100 < y2){
+                if (y1 + 100 < y2) {
                     Intent i = new Intent(MainActivity.this, Description.class);
                     startActivity(i);
                 }
