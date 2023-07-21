f052f3a77630a877c7199e162a4e5040ac1808ef
Vladislav Famtsov <gggwp4@gmail.com~>
2023-07-21 12:31:50 +0300

    realm connected

---
+++ JavaApp/app/src/main/java/com/example/javaapp/Sorry.java
@@ -0,0 +1,16 @@
+package com.example.javaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Sorry extends AppCompatActivity {
+    private TextView sorry_text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sorry);
+        sorry_text = findViewById(R.id.sorry_text);
+
+    }
+}
